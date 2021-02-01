2c3a73db58eee8600bf1b98331b05cccd329da29
GabrielVander <gabsvander@hotmail.com>
2021-01-31 23:22:20 -0300

    Heroku setup

---
+++ server.js
@@ -0,0 +1,15 @@
+//Install express server
+const express = require('express');
+
+const app = express();
+
+// Serve only the static files form the dist directory
+app.use(express.static('./dist/tc2-assignment-frontend'));
+
+app.get('/*', function (req, res) {
+  res.sendFile('index.html', {root: 'dist/tc2-assignment-frontend/'}
+  );
+});
+
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
